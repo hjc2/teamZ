26d7f4b01d87f3c0eedfccc77b10d00b96b94467
hcolematx <hughjcoleman@gmail.com>
2020-07-15 20:23:56 -0500

    snail branch

--- include/285Z/robot/functions.h
+++ include/285Z/robot/functions.h
@@ -1,5 +1,7 @@
 #include "main.h"
 
 //SNAILBOT DEMO CODE
-
+bool intakeControl(bool overRide, bool io);
+bool fwControl(bool overRide, bool io);
+bool beltControl(bool overRide, bool io);
 
--- src/robot/functions.cpp
+++ src/robot/functions.cpp
@@ -1,3 +1,14 @@
 #include "main.h"
 #include "../include/285Z/robot/functions.h"
 
+bool intakeControl(bool overRide, bool io){
+    
+}
+
+bool fwControl(bool overRide, bool io){
+
+}
+
+bool beltControl(bool overRide, bool io){
+
+}
