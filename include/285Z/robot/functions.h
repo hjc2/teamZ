4563306a1303e7f3d7298bc68c8cd7a0fd66df3c
hcolematx <hughjcoleman@gmail.com>
2020-07-15 20:18:58 -0500

    new branch

--- include/285Z/robot/functions.h
+++ include/285Z/robot/functions.h
@@ -1,7 +1,5 @@
 #include "main.h"
 
 //SNAILBOT DEMO CODE
-bool toggleIntake(){}
-bool toggleFW(){}
-bool pushBelt(){}
+
 
--- src/robot/functions.cpp
+++ src/robot/functions.cpp
@@ -1,14 +1,3 @@
 #include "main.h"
 #include "../include/285Z/robot/functions.h"
 
-//SNAILBOT DEMO CODE
-
-bool toggleIntake(){
-    //toggle intake motor
-}
-bool toggleFW(){
-    //toggle fw motor
-}
-bool pushBelt(){
-    //toggle 
-}
