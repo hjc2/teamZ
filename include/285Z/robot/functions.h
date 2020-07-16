351e09702f210ba7d526eec392668d09879cceea
hcolematx <hughjcoleman@gmail.com>
2020-07-16 15:07:52 -0500

    added extern to the the toggler bools (fixed redefinition error)

--- include/285Z/robot/functions.h
+++ include/285Z/robot/functions.h
@@ -5,6 +5,6 @@ bool intakeControl(bool overRide, int io);
 bool fwControl(bool overRide, int io);
 bool beltControl(bool overRide, int io);
 
-bool fwToggler;
-bool intakeToggler;
-bool beltToggler;
+extern bool fwToggler;
+extern bool intakeToggler;
+extern bool beltToggler;
