dfe55a687bb514130f98cb5df08187e110a7e2c4
hcolemantx <46972988+hcolemantx@users.noreply.github.com>
2020-09-16 14:36:49 -0500

    working on mp
    asdfadf

--- src/main.cpp
+++ src/main.cpp
@@ -15,25 +15,46 @@ void autonomous() {
 	auto autoChassis = ChassisControllerBuilder()
 	.withMotors(2, -3, -5, 4) //tl, tr, br, bl //  .withMotors(frontLeftMotor, frontRightMotor, backRightMotor, backLeftMotor)
 	.withDimensions(AbstractMotor::gearset::green, {{15_in, 15_in}, imev5GreenTPR})
-	.build();
+	.withOdometry()
+	.buildOdometry();
     auto xModel = std::dynamic_pointer_cast<XDriveModel>(autoChassis->getModel());
-	xModel->xArcade(1,0,0);
+	autoChassis->setState({0_in, 0_in, 0_deg});
+	pros::delay(20);
+	autoChassis->driveToPoint({1_ft, 1_ft});
+	//autoChassis->turnToAngle(90_deg);
+	//autoChassis.waitUntilSettled();
+
+	/*
+	while(1){
+		xModel->xArcade(0.1,0,0);
+		pros::delay(1000);
+		xModel->xArcade(0,0.1,0);
+		pros::delay(1000);
+		//xModel->xArcade(0,0.1,0);
+		//pros::delay(1000);
+	}*/
 }
 
 void opcontrol() {
-	//XDRIVE CHASSIS GENERATION
+
+	//DO NOT TOUCH THIS CODE
     auto chassis = ChassisControllerBuilder()
 	.withMotors(frontLeftMotorPort, frontRightMotorPort, backRightMotorPort, backLeftMotorPort) //tl, tr, br, bl //  .withMotors(frontLeftMotor, frontRightMotor, backRightMotor, backLeftMotor)
 	.withDimensions(AbstractMotor::gearset::green, {{15_in, 15_in}, imev5GreenTPR})
 	.build();
     auto xModel = std::dynamic_pointer_cast<XDriveModel>(chassis->getModel());
     Controller controller(ControllerId::master);
-	//WHILE LOOP FOR OP
+	//DO NOT TOUCH THIS CODE
+
+	//WHILE LOOP FOR OPCONTROL
     while (true) {
+		//DO NOT TOUCH THIS CODE
         xModel->xArcade(
 			controller.getAnalog(ControllerAnalog::rightX), //side to side
         	controller.getAnalog(ControllerAnalog::rightY), //front back
         	controller.getAnalog(ControllerAnalog::leftX) //spin
 		);
+		//DO NOT TOUCH THIS CODE
+		pros::delay(20);
 	}
 }
