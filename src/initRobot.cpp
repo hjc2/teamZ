8b68eb1c6a2b08e7dd023735740e59d914b931fa
hcolemantx <46972988+hcolemantx@users.noreply.github.com>
2020-09-16 11:35:22 -0500

    xDrive
    daksjflkjf

--- src/initRobot.cpp
+++ src/initRobot.cpp
@@ -10,10 +10,10 @@ int portF = 5;
 int portG = 6;
 int portH = 7;
 
-pros::Motor driveMotorA(portA);
-pros::Motor driveMotorB(portB);
-pros::Motor driveMotorC(portC);
-pros::Motor driveMotorD(portD);
+//pros::Motor driveMotorA(portA);
+//pros::Motor driveMotorB(portB);
+//pros::Motor driveMotorC(portC);
+//pros::Motor driveMotorD(portD);
 pros::Motor driveMotorE(portE);
 pros::Motor driveMotorF(portF);
 pros::Motor driveMotorG(portG);
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "../include/285Z/initRobot.h"
+using namespace okapi;
 
 void on_center_button() { }
 
@@ -12,7 +13,17 @@ void competition_initialize() {}
 void autonomous() {}
 
 void opcontrol() {
-	while (true) {
-		pros::delay(portA);
+    auto chassis = ChassisControllerBuilder()
+	.withMotors(2, -3, -5, 4) //tl, tr, br, bl //  .withMotors(frontLeftMotor, frontRightMotor, backRightMotor, backLeftMotor)
+	.withDimensions(AbstractMotor::gearset::green, {{15_in, 15_in}, imev5GreenTPR})
+	.build();
+    auto xModel = std::dynamic_pointer_cast<XDriveModel>(chassis->getModel());
+    Controller controller(ControllerId::master);
+    while (true) {
+        xModel->xArcade(
+			controller.getAnalog(ControllerAnalog::rightX), //side to side
+        	controller.getAnalog(ControllerAnalog::rightY), //front back
+        	controller.getAnalog(ControllerAnalog::leftX) //spin
+			);
 	}
 }
