f114d2b08a39b8b83f62ef1cd79edfa9ebcf0774
hcolemantx <46972988+hcolemantx@users.noreply.github.com>
2020-09-18 09:17:34 -0500

    i made a branch with no odom
    no odom

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,17 @@
 #include "main.h"
 #include "../include/285Z/initRobot.h"
-using namespace okapi;
+using namespace okapi; //DONT TOUCH THIS
 
 void on_center_button() { }
 
 void initialize() {}
 
-void disabled() {}
+void disabled() {} //LEAVE THIS EMPTY
 
 void competition_initialize() {}
 
 //xArcade(myStrafe, myForward, mySpin)
 void autonomous() {
-	/*
-	auto autoChassis = ChassisControllerBuilder()
-	.withMotors(2, -3, -5, 4) //tl, tr, br, bl //  .withMotors(frontLeftMotor, frontRightMotor, backRightMotor, backLeftMotor)
-	.withDimensions(AbstractMotor::gearset::green, {{15_in, 15_in}, imev5GreenTPR})
-	.withOdometry()
-	.buildOdometry();
-    auto xModel = std::dynamic_pointer_cast<XDriveModel>(autoChassis->getModel());
-	autoChassis->setState({0_in, 0_in, 0_deg});
-	pros::delay(20);
-	autoChassis->driveToPoint({1_ft, 1_ft});
-	*/
-	//autoChassis->turnToAngle(90_deg);
-	//autoChassis.waitUntilSettled();
-
-	/*
-	while(1){
-		xModel->xArcade(0.1,0,0);
-		pros::delay(1000);
-		xModel->xArcade(0,0.1,0);
-		pros::delay(1000);
-		//xModel->xArcade(0,0.1,0);
-		//pros::delay(1000);
-	}*/
 }
 
 void opcontrol() {
