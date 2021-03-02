c8880a568444922394328aced22aa72ef9aa0661
jared2183 <43629112+zougat@users.noreply.github.com>
2021-03-02 13:19:32 -0600

    Update miscAuton.cpp

--- src/autonomous/miscAuton.cpp
+++ src/autonomous/miscAuton.cpp
@@ -23,8 +23,8 @@ void skillsAuto(){ //SKILLS AUTO
   AsyncMotionProfileControllerBuilder()
     .withLimits({
       1.2, // Maximum linear velocity of the Chassis in m/s
-      1.1, // Maximum linear acceleration of the Chassis in m/s/s
-      4.0 // Maximum linear jerk of the Chassis in m/s/s/s
+      2.0, // Maximum linear acceleration of the Chassis in m/s/s
+      10.0 // Maximum linear jerk of the Chassis in m/s/s/s
     })
     .withOutput(chassis)
     .buildMotionProfileController();
